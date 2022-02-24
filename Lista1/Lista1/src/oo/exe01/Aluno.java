171ec51b4700e8402e7d44a6d152016ae5e3bbcc
Amanda Barboza <71797931+amandabarboza@users.noreply.github.com>
2022-02-24 13:25:38 -0300

    24-02-2022

--- Lista1/Lista1/src/oo/exe01/Aluno.java
+++ Lista1/Lista1/src/oo/exe01/Aluno.java
@@ -1,3 +1,19 @@
+/*
+Exercício 1: 
+
+•	Crie uma classe em Java chamada Aluno contendo as variáveis de instância numeroAluno (int), nome (String), idade (int), p1 (float) e p2 (float), 
+
+•	Crie os construtores 
+
+•	Crie os métodos notaFinal() - que calcula e retorna a média final do aluno - , e dadosAluno() - que retorna os valores de numeroAluno, nome e idade. 
+
+•	Criar uma classe TestaAluno com o método main() para instanciação da classe Aluno (atribuição das variáveis de instância com quaisquer valores e chamada dos métodos notaFinal() e dadosAluno()).  
+
+•	Adicionar o método passou() à classe Aluno a fim de verificar se o aluno passou. Chame o método passou() na classe TestaAluno a fim de testa-lo.
+
+*/
+
+
 package oo.exe01;
 
 import javax.swing.JOptionPane;
